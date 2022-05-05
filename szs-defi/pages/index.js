2212d7044d7ec529277f5257a4fd9204420fab38
zainamroti <sayedzainjeelani@gmail.com>
2022-05-05 10:24:45 +0500

    fix css and change network to ropsten

--- szs-defi/pages/index.js
+++ szs-defi/pages/index.js
@@ -285,11 +285,11 @@ export default function Home() {
     const provider = await web3ModalRef.current.connect();
     const web3Provider = new providers.Web3Provider(provider);
 
-    // If user is not connected to the Rinkeby network, let them know and throw an error
+    // If user is not connected to the Ropsten network, let them know and throw an error
     const { chainId } = await web3Provider.getNetwork();
-    if (chainId !== 4) {
-      window.alert("Change the network to Rinkeby");
-      throw new Error("Change network to Rinkeby");
+    if (chainId !== 3) {
+      window.alert("Change the network to Ropsten");
+      throw new Error("Change network to Ropsten");
     }
 
     if (needSigner) {
@@ -308,7 +308,7 @@ export default function Home() {
       // Assign the Web3Modal class to the reference object by setting it's `current` value
       // The `current` value is persisted throughout as long as this page is open
       web3ModalRef.current = new Web3Modal({
-        network: "rinkeby",
+        network: "ropsten",
         providerOptions: {},
         disableInjectedProvider: false,
       });
